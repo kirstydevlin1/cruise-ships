79d5563172ad3641d402051cf2df769a353d0335
Reetu Child <rdhir79@hotmail.co.uk>
2018-03-17 17:09:43 +0000

    Tests for Ship and Ship constructor

--- _tests_/Ship.test.js
+++ _tests_/Ship.test.js
@@ -1,12 +1,47 @@
+/* eslint-env jest */
 const Ship = require('../src/Ship.js');
+const Port = require('../src/Port.js');
+const Itinerary = require('../src/Itinerary.js');
+
+
 describe('Ship', () => {
-  it('can be instantiated', ()=> {
-    expect(new Ship()).toBeInstanceOf(Object);
+  it('can be instantiated', () => {
+    const port = new Port('Dover');
+    const itinerary = new Itinerary([port]);
+    const ship = new Ship(itinerary);
+    expect(ship).toBeInstanceOf(Object);
   });
+
   it('has a starting port', () => {
-    const ship = new Ship('Dover');
-    expect(ship.startingPort).toBe('Dover');
-  
+    const port = new Port('Dover');
+    const itinerary = new Itinerary([port]);
+    const ship = new Ship(itinerary);
+    expect(ship.currentPort).toBe(port);
+  });
+
+  it('can set sail', () => {
+    const port = new Port('Dover');
+    const itinerary = new Itinerary([port]);
+    const ship = new Ship(itinerary);
+    ship.setSail();
+    expect(port.ships).not.toContain(ship);
+    expect(ship.currentPort).toBeFalsy();
+  });
+
+  it('can dock at a different port', () => {
+    const dover = new Port('Dover');
+    const calais = new Port('Calais');
+    const itinerary = new Itinerary([dover, calais]);
+    const ship = new Ship(itinerary);
+    ship.dock();
+    expect(ship.currentPort).toBe(calais);
+    expect(calais.ships).toContain(ship);
+  });
+
+  it('can be added to a port on instantiation', () => {
+    const port = new Port('Dover');
+    const itinerary = new Itinerary([port]);
+    const ship = new Ship(itinerary);
+    expect(port.ships).toContain(ship);
   });
 });
- 
--- src/Ship.js
+++ src/Ship.js
@@ -1,6 +1,19 @@
-function Ship(startingPort) {
-    this.startingPort = startingPort
+function Ship(itinerary) {
+  this.itinerary = itinerary;
+  this.currentPort = itinerary.ports[0];
+  this.currentPort.addShip(this);
 }
-  
 
+Ship.prototype = {
+  setSail() {
+    this.currentPort.removeShip(this);
+    this.currentPort = false;
+  },
+  dock() {
+    const itinerary = this.itinerary;
+    const previousPortIndex = itinerary.ports.indexOf(this.currentPort);
+    this.currentPort = itinerary.ports[previousPortIndex + 1];
+    this.currentPort.addShip(this);
+  },
+};
 module.exports = Ship;
