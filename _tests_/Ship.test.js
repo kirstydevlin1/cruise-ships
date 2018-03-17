88bc0f50fb05b10b084b56215c2ae4a69d2c5a7a
Reetu Child <rdhir79@hotmail.co.uk>
2018-03-17 19:24:20 +0000

    Tests for Ships refactored using beforeEach

--- _tests_/Ship.test.js
+++ _tests_/Ship.test.js
@@ -5,28 +5,36 @@ const Itinerary = require('../src/Itinerary.js');
 
 
 describe('Ship', () => {
-  it('can be instantiated', () => {
-    const port = new Port('Dover');
-    const itinerary = new Itinerary([port]);
-    const ship = new Ship(itinerary);
-    expect(ship).toBeInstanceOf(Object);
+  describe('with a port and itinerary', () => {
+    let ship;
+    let port;
+
+    beforeEach(() => {
+      port = new Port('Dover');
+      const itinerary = new Itinerary([port]);
+      ship = new Ship(itinerary);
+    });
+
+    it('can be instantiated', () => {
+      expect(ship).toBeInstanceOf(Object);
+    });
+
+    it('has a starting port', () => {
+      expect(ship.currentPort).toBe(port);
+    });
+
+    it('can set sail', () => {
+      ship.setSail();
+
+      expect(port.ships).not.toContain(ship);
+      expect(ship.currentPort).toBeFalsy();
+    });
+
+    it('can be added to a port on instantiation', () => {
+      expect(port.ships).toContain(ship);
+    });
   });
 
-  it('has a starting port', () => {
-    const port = new Port('Dover');
-    const itinerary = new Itinerary([port]);
-    const ship = new Ship(itinerary);
-    expect(ship.currentPort).toBe(port);
-  });
-
-  it('can set sail', () => {
-    const port = new Port('Dover');
-    const itinerary = new Itinerary([port]);
-    const ship = new Ship(itinerary);
-    ship.setSail();
-    expect(port.ships).not.toContain(ship);
-    expect(ship.currentPort).toBeFalsy();
-  });
 
   it('can dock at a different port', () => {
     const dover = new Port('Dover');
@@ -34,14 +42,8 @@ describe('Ship', () => {
     const itinerary = new Itinerary([dover, calais]);
     const ship = new Ship(itinerary);
     ship.dock();
+
     expect(ship.currentPort).toBe(calais);
     expect(calais.ships).toContain(ship);
-  });
-
-  it('can be added to a port on instantiation', () => {
-    const port = new Port('Dover');
-    const itinerary = new Itinerary([port]);
-    const ship = new Ship(itinerary);
-    expect(port.ships).toContain(ship);
   });
 });
